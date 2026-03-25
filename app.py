55943710546c4e28dc93fc5bc26508fb479eea20
krishna <krishna04@gmail.com>
2026-03-25 13:07:42 +0000

    python-code

---
+++ app.py
@@ -0,0 +1,8 @@
+eno=1001
+ename="krishna"
+course="devops"
+print("Learn here and lead anywhere")
+print(f' emp name is:{ename}')
+print(f' emp number is:{eno}')
+print(f' emp name is:{course}')
+print("thanks for using our app")
